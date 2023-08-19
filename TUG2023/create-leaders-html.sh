ff5986edfc17fb4a75b91a0d8f44e28bf3cbbf25
Norbert Preining <norbert@preining.info>
2023-08-19 16:47:37 +0900

    Start TUG2023

---
+++ TUG2023/create-leaders-html.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+mkdir -p LB
+
+# token,Lecturer,Title,ZoomRecording,LogoPosition,Part 1 Start,Part 1 End,PreRecordedFileName,Part 3 Start,Part 3 End,Comment,Timing by,Double checked by
+while IFS=, read -r token lecturer title zoomfile foobar part1start part1end prerec part3start part3end comment rest ; do
+  #echo -e "token=$token\nzoomfile=$zoomfile\nstart1=$part1start\nprerec=$prerec"
+  if [ -z "$token" ] ; then
+    echo "Missing token for line"
+    continue
+  fi
+  if [ -z "$zoomfile" ] ; then
+    echo "Missing zoom file for token $token"
+    continue
+  fi
+  if [ -z "$part1start" ] ; then
+    echo "Missing part 1 start for token $token"
+    continue
+  fi
+  if [ -z "$part1end" ] ; then
+    echo "Missing part 1 end for token $token"
+    continue
+  fi
+  PART1FILE="$zoomfile"
+  PART1START="$part1start"
+  PART1END="$part1end"
+  PRERECORDED="$prerec"
+  PART3FILE="$part3start"
+  PART3START="$part3start"
+  PART3END="$part3end"
+  #
+  foo=${PART1FILE#GMT}
+  foo=${foo%-*}
+  ds=`date -d$foo +"%b. %d, 2023"`
+  if [ -r "LB/$token.html" ] ; then
+    echo "Renaming old LB $token.html to $token.previous.html in LB directory."
+    mv "LB/$token.html" "LB/$token.previous.html"
+  fi
+  title=`echo $title | sed -e 's!&!\\\\&!g'`
+  sed -e "s!__AUTHORS__!$lecturer!g" \
+      -e "s!__TITLE__!$title!g" \
+      -e "s!__DATE__!$ds!g" \
+    sources/leader-board.html > "LB/$token.html"
+done < "TUG Meetings Video Data Sheet - 2023.csv"
+
+
+# :set tabstop=2 shiftwidth=2 expandtab
---
+++ TUG2023/create-leaders-mp4.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir -p leaders
+for i in LB/*.html ; do
+  slug=$(basename $i .html)
+  output=leaders/$slug.mp4
+  if [ -r $output ] ; then
+    echo "Output already exists, not recreating $output" >&2
+    continue
+  fi
+  snapdir=$(mktemp -d)
+  node timesnap/node_modules/timesnap/cli.js -L "--no-sandbox --disable-gpu" $i --viewport=1920,1080 --fps=25 --duration=10 --output-pattern="$snapdir/leader-%03d.png"
+  ffmpeg -r 25 -f image2 -s 1920x1080 -i $snapdir/leader-%03d.png -vcodec libx264 -crf 25 -pix_fmt yuv420p $output
+  rm $snapdir/leader-*.png
+  rmdir $snapdir
+done
